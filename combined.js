5e903adad265d6b3148276a41f2735830308d0fa
git-kur <141483325+git-kur@users.noreply.github.com>
2023-08-08 03:18:49 +0530

    Create combined.js

---
+++ combined.js
@@ -0,0 +1,55 @@
+     $('.glint-button-container').click(function() {
+       $('.glint-button-container').css('pointer-events', 'none');
+       var overlay_navigation = $('.overlay-navigation'),
+         top_bar = $('.bar-top'),
+         middle_bar = $('.bar-middle'),
+         bottom_bar = $('.bar-bottom');
+
+       overlay_navigation.toggleClass('overlay-active');
+       if (overlay_navigation.hasClass('overlay-active')) {
+
+   
+         overlay_navigation.velocity('transition.slideLeftIn', {
+           duration: 300,
+           delay: 0,
+           begin: function() {
+             $('nav ul li').velocity('transition.perspectiveLeftIn', {
+               stagger: 150,
+               delay: 0,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [1, 0],
+                 }, {
+                   delay: 10,
+                   duration: 140
+                 });
+                 $('.glint-button-container').css('pointer-events', 'auto');
+               }
+             })
+           }
+         })
+
+       } else {
+         $('.glint-button-container').css('pointer-events', 'none');
+        
+         $('nav ul li').velocity('transition.perspectiveRightOut', {
+           stagger: 150,
+           delay: 0,
+           complete: function() {
+             overlay_navigation.velocity('transition.fadeOut', {
+               delay: 0,
+               duration: 300,
+               complete: function() {
+                 $('nav ul li a').velocity({
+                   opacity: [0, 1],
+                 }, {
+                   delay: 0,
+                   duration: 50
+                 });
+                 $('.open-overlay').css('pointer-events', 'auto');
+               }
+             });
+           }
+         })
+       }
+     })
