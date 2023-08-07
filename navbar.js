7c161bd3e2c44c0ec25119eafb2dfcd011ae00cb
git-kur <141483325+git-kur@users.noreply.github.com>
2023-08-07 17:26:36 +0530

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,62 +1,68 @@
-$(document).ready(function() {
-  $('.open-overlay').click(function() {
-    $('.open-overlay').css('pointer-events', 'none');
-    var overlay_navigation = $('.overlay-navigation'),
-      top_bar = $('.bar-top'),
-      middle_bar = $('.bar-middle'),
-      bottom_bar = $('.bar-bottom');
+<!-- Add the latest version of the Velocity.js library -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/velocity/2.0.5/velocity.min.js"></script>
 
-    overlay_navigation.toggleClass('overlay-active');
-    if (overlay_navigation.hasClass('overlay-active')) {
-      top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
-      middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
-      bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
-      overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down');
-
-      overlay_navigation.velocity('slideDown', { // Use 'slideDown' instead of 'transition.slideLeftIn'
-        duration: 300,
-        delay: 0,
-        begin: function() {
-          $('nav ul li').velocity('transition.perspectiveLeftIn', {
-            stagger: 150,
-            delay: 0,
-            complete: function() {
-              $('nav ul li a').velocity({
-                opacity: [1, 0],
-              }, {
-                delay: 10,
-                duration: 140
-              });
-              $('.open-overlay').css('pointer-events', 'auto');
-            }
-          });
-        }
-      });
-    } else {
+<!-- Your jQuery and other scripts go here -->
+<script>
+  $(document).ready(function() {
+    $('.open-overlay').click(function() {
       $('.open-overlay').css('pointer-events', 'none');
-      top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
-      middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
-      bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
-      overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up');
-      $('nav ul li').velocity('transition.perspectiveRightOut', {
-        stagger: 150,
-        delay: 0,
-        complete: function() {
-          overlay_navigation.velocity('fadeOut', { // Use 'fadeOut' instead of 'transition.fadeOut'
-            delay: 0,
-            duration: 300,
-            complete: function() {
-              $('nav ul li a').velocity({
-                opacity: [0, 1],
-              }, {
-                delay: 0,
-                duration: 50
-              });
-              $('.open-overlay').css('pointer-events', 'auto');
-            }
-          });
-        }
-      });
-    }
+      var overlay_navigation = $('.overlay-navigation'),
+        top_bar = $('.bar-top'),
+        middle_bar = $('.bar-middle'),
+        bottom_bar = $('.bar-bottom');
+
+      overlay_navigation.toggleClass('overlay-active');
+      if (overlay_navigation.hasClass('overlay-active')) {
+        top_bar.removeClass('animate-out-top-bar').addClass('animate-top-bar');
+        middle_bar.removeClass('animate-out-middle-bar').addClass('animate-middle-bar');
+        bottom_bar.removeClass('animate-out-bottom-bar').addClass('animate-bottom-bar');
+        overlay_navigation.removeClass('overlay-slide-up').addClass('overlay-slide-down');
+
+        overlay_navigation.velocity('slideDown', {
+          duration: 300,
+          delay: 0,
+          begin: function() {
+            $('nav ul li').velocity('fadeIn', {
+              stagger: 150,
+              delay: 0,
+              complete: function() {
+                $('nav ul li a').velocity({
+                  opacity: [1, 0],
+                }, {
+                  delay: 10,
+                  duration: 140
+                });
+                $('.open-overlay').css('pointer-events', 'auto');
+              }
+            });
+          }
+        });
+      } else {
+        $('.open-overlay').css('pointer-events', 'none');
+        top_bar.removeClass('animate-top-bar').addClass('animate-out-top-bar');
+        middle_bar.removeClass('animate-middle-bar').addClass('animate-out-middle-bar');
+        bottom_bar.removeClass('animate-bottom-bar').addClass('animate-out-bottom-bar');
+        overlay_navigation.removeClass('overlay-slide-down').addClass('overlay-slide-up');
+        $('nav ul li').velocity('fadeOut', {
+          stagger: 150,
+          delay: 0,
+          complete: function() {
+            overlay_navigation.velocity('fadeOut', {
+              delay: 0,
+              duration: 300,
+              complete: function() {
+                $('nav ul li a').velocity({
+                  opacity: [0, 1],
+                }, {
+                  delay: 0,
+                  duration: 50
+                });
+                $('.open-overlay').css('pointer-events', 'auto');
+              }
+            });
+          }
+        });
+      }
+    });
   });
-});
+</script>
